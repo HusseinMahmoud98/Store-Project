a18a44cccf639ca854f573404644331223704e7d
HusseinMahmoud98 <hussein.mahmoud.fouad98@gmail.com>
2025-11-29 03:03:15 +0200

    Add Attribute folder to project into repo

---
+++ Infrastructure/Store.Presentation/Attributes/CacheAttribute.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using Store.Domain.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Presentation.Attributes
+{
+    public class CacheAttribute(int timeInSec) : Attribute, IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var cacheService = context.HttpContext.RequestServices.GetRequiredService<IServiceManager>().CacheService;
+            //Generate key
+            var cacheKey = GetCasheKey(context.HttpContext.Request);
+
+            var result = await cacheService.GetAsync(cacheKey);
+
+            if (!string.IsNullOrEmpty(result)) 
+            {
+                //Return Response
+                var response = new ContentResult()
+                {
+                    Content = result,
+                    ContentType = "application/json",
+                    StatusCode = 200
+                };
+
+                context.Result = response;
+                return;
+            }
+
+            var actionContext = await next.Invoke();
+
+            if (actionContext.Result is OkObjectResult okObjectResult)
+            {
+                await cacheService.SetAsync(cacheKey, okObjectResult.Value, TimeSpan.FromSeconds(timeInSec));
+            }
+        }
+
+        private string GetCasheKey(HttpRequest request)
+        {
+            var key = new StringBuilder();
+            key.Append(request.Path);
+
+            foreach (var item in request.Query)
+            {
+                key.Append($"|{item.Key}-{item.Value}");   
+            }
+
+            return key.ToString();
+        }
+    }
+}
